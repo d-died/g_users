291bda493d02dcb22b2f28cb74a744025b9a4d8a
dsump <dsumpmann@gmail.com>
2022-05-11 11:58:57 -0600

    added dummy json server

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -16,23 +16,28 @@ const UserType = new GraphQLObjectType({
     }
 });
 
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '24', firstName: 'Sam', age: 45 }
-]
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
             type: UserType,
-            // if you give GraphQL a user ID, is will return all the properties of a given user (aka UserType)
+            // if you give GraphQL a user ID, it will return all the properties of a given user (aka UserType)
+            // essentially, this "args" is THE argument you're giving GraphQL to find
             args: { id: { type: GraphQLString } },
             // the below resolve() function is where we actually go into the data and find data we're looking for
             // parentValue is not really used ever. 
             // args argument is an object that gets passed into the original query aka args from above
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                // return _.find(users, { id: args.id });
+                // return Array.prototype.find(users, { id: args.id });
+                // return users.find(user => user.id === args.id);
+                // just some different ways we can make the same query -- the very first example we did
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(res => res.data)
+                    // this is needed when pairing axios and GraphQL. 
+                    // Axios sends a response object named "data", but GraphQL doesn't know what to do with that.
+                    // so we just dot into that object and can manipulate the response object from there
             }
         }
     }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // this is telling express that when we make a query with /graphql URL, to run it through expressGraphQL
 // any time we need to direct incoming requests to another application (aka middleware), we run .use() method. 
 app.use('/graphql', expressGraphQL({
-    schema,
+    schema: schema,
     graphiql: true
 }));
 
